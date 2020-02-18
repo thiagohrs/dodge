5bbe25068693183b5f0db6e6beee040d055ca631
thiagohrs <thiagohrs@gmail.com>
2020-02-18 15:28:41 -0300

    Commit inicial

---
+++ Mob.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+export (int) var min_speed
+export (int) var max_speed
+
+var mob_types = ["walk", "swim", "fly"]
+
+func _ready():
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	show()
+
+
+func _on_Visibility_screen_exited():
+	queue_free()
